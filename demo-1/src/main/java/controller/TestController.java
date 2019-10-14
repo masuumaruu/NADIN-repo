e0c522d4ef06534d1581a6457f02e2747ddd0908
Ralph <ralph@gmail.com>
2019-10-14 08:19:31 +0800

    First commit

---
+++ demo-1/src/main/java/controller/TestController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+
+	@GetMapping
+	public String home()
+	{
+		return "test";
+	}
+}
